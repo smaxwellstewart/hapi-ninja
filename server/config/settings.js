8cd827126506d38f6c29a39416fe0f1df996790d
smaxwellstewart <smaxwellstewart@gmail.com>
2014-09-30 12:34:47 +0200

    changed port to 3333

--- server/config/settings.js
+++ server/config/settings.js
@@ -7,7 +7,7 @@ rootPath = path.normalize(__dirname + '/../..');
 // Defaults that you can access when you require this config.
 module.exports = {
     root: rootPath,
-    port: parseInt(process.env.PORT, 10) || 3100,
+    port: parseInt(process.env.PORT, 10) || 3333,
     hapi: {
         options: {
             views: {
